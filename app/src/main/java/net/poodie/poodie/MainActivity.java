81abaff2f501213b985916f0ea84943949b99f76
Zarni Phyo <zarniphyo95@gmail.com>
2017-01-14 21:20:25 -0500

    feat: Add dependencies, UI components, and MainActivity

---
+++ app/src/main/java/net/poodie/poodie/MainActivity.java
@@ -0,0 +1,16 @@
+package net.poodie.poodie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Zarni Phyo on 1/14/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
